6ac046fc8a33aa7f425473dae6668604c6853d62
clément cyril <cyril@clmntcrl.io>
2018-10-31 12:25:41 +0100

    Add iOS project to run on device (or simulator)

---
+++ Tasks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//  Created by Cyril Clément
+//  Copyright © 2018 clmntcrl. All rights reserved.
+
+import UIKit
+
+import SwiduxTasksExample
+import SwiduxRouter
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let window = UIWindow()
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window.rootViewController =  Router(
+            store: store,
+            keyPath: \.routes,
+            initialRoute: AppRoute.tasks(withParentTaskId: .root)
+        )
+        window.makeKeyAndVisible()
+        return true
+    }
+}
